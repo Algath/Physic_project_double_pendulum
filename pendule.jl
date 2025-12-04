6a5c2c077e591822350ce45252e566225dc131f7
Algath <n3177713@gmail.com>
2025-12-04 11:32:46 +0100

    Collecting datas

---
+++ pendule.jl
@@ -0,0 +1,33 @@
+using HypertextLiterals
+using PlutoUI
+
+md"""
+# Double pendulum simulation
+
+This is a simulation of a double pendulum using Julia.
+
+The objectif is too recreate a double pendulum simulation based on videos and to predict his mouvement during 2 seconds past the video end.
+"""
+
+md"""
+## Theory
+A double pendulum is also know as Chaotic pendulum because of its sensibility to initial conditions.
+
+The particularity of this system is his behavior. It can vary from simple periodic motion to a more complex and chaotic one with a small change of initial angles or velocities.
+
+"""
+
+md"""
+## length of the pendulums (via gimps)
+pendulum one = 
+pendulum two = 
+base = 
+"""
+
+md"""
+## Parameters
+- length of the first pendulum: L1 =  cm
+- length of the second pendulum: L2 =  cm
+- angles at t=0: θ1 = 181.5° = 3.159, θ2 = 183.1° = 3.194 (θ = 0° is the vertical down position)
+"""
+
